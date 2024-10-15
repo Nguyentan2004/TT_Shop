ffb9a5a751b59fa954103e11fb60ca0dae0cafe3
Nguyentan2004 <nhattan2004xd@gmail.com>
2024-10-15 23:01:24 +0700

    6

--- TT_Shop/TT_Shop/Controllers/HomeController.cs
+++ TT_Shop/TT_Shop/Controllers/HomeController.cs
@@ -3,27 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TT_Shop.Models;
 
 namespace TT_Shop.Controllers
 {
     public class HomeController : Controller
     {
+        private QLTTShopEntities db = new QLTTShopEntities();
+
         public ActionResult Index()
         {
-            return View();
+            var allProducts = db.Products.ToList();
+            return View(allProducts);
         }
+
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
