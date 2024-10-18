2cccb65cac785ce82c9cb18948a3297313fa3bd7
LâmLmao <thanhlam03082004@gmail.com>
2024-10-18 10:58:25 +0700

    admin vs cart tam thoi

--- TT_Shop/TT_Shop/Controllers/UserController.cs
+++ TT_Shop/TT_Shop/Controllers/UserController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+#if !IGNORE_TT_SHOP_MODEL
 using TT_Shop.Model;
+#endif
 
 
 namespace TT_Shop.Controllers
