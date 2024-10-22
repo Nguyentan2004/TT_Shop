6233be1e4b8a8de172ce39b44f89085c67dcc624
Nguyentan2004 <nhattan2004xd@gmail.com>
2024-10-22 23:12:25 +0700

    14

---
+++ TT_Shop/TT_Shop/Controllers/CartController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TT_Shop.Models;
+
+namespace TT_Shop.Controllers
+{
+    public class CartController : Controller
+    {
+        private QLTTShopEntities db = new QLTTShopEntities();
+
+        // GET: Cart
+        //public ActionResult Index()
+        //{
+        //    // Lấy các mục trong giỏ hàng từ cơ sở dữ liệu
+        //    List<CartItem> cartItems = db.CartItems.ToList();
+        //    return View(cartItems);
+        //}
+    }
+}
