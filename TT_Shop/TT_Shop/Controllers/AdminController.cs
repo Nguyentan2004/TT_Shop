d6b029f3572e9e300858d4f01a48bd0a49dfed48
Nguyentan2004 <nhattan2004xd@gmail.com>
2024-11-23 22:38:46 +0700

    AddAdmin

---
+++ TT_Shop/TT_Shop/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+
+namespace TTshop.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Action to navigate to AccountsView
+        public ActionResult AccountsView()
+        {
+            return View("~/Views/AccountManagement/AccountsView.cshtml");
+        }
+
+        // Action to navigate to ProductsView
+        public ActionResult ProductsView()
+        {
+            return View("~/Views/ProductManagement/ProductsView.cshtml");
+        }
+
+        // Action to navigate to ReportsView
+        public ActionResult ReportsView()
+        {
+            return View("~/Views/Report/ReportsView.cshtml");
+        }
+
+        // Action to navigate to TransactionHistoryView
+        public ActionResult TransactionHistoryView()
+        {
+            return View("~/Views/TransactionHistory/TransactionHistoryView.cshtml");
+        }
+    }
+}
---
+++ TT_Shop/TT_Shop/Controllers/OrdersController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TT_Shop.Models;
+
+namespace TTshop.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private QLTTShopEntities db = new QLTTShopEntities();
+
+        // GET: Orders
+        public async Task<ActionResult> Index()
+        {
+            var orders = db.Orders.Include(o => o.User);
+            return View(await orders.ToListAsync());
+        }
+
+        // GET: Orders/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = await db.Orders
+                .Include(o => o.Order_Details.Select(od => od.Product))
+                .Include(o => o.User)
+                .FirstOrDefaultAsync(o => o.order_id == id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.user_id = new SelectList(db.Users, "user_id", "fullname");
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "order_id,user_id,order_status,total_amount,shipping_address,order_date,updated_at")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.user_id = new SelectList(db.Users, "user_id", "fullname", order.user_id);
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = await db.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.user_id = new SelectList(db.Users, "user_id", "fullname", order.user_id);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "order_id,user_id,order_status,total_amount,shipping_address,order_date,updated_at")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.user_id = new SelectList(db.Users, "user_id", "fullname", order.user_id);
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = await db.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Order order = await db.Orders.FindAsync(id);
+            db.Orders.Remove(order);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
